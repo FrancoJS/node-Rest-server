bf45ad1dfd085ccd541b2aa9821560bd88116bd3
Franco <jean.jara.fs@gmail.com>
2020-12-02 21:35:11 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,13 @@
+//Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+//Base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === "dev") urlDB = "mongodb://localhost:27017/cafe";
+else urlDB = "mongodb+srv://Jean_Admin:A5wi3T9DUIcQkMAJ@cluster0.aihil.mongodb.net/cafe";
+
+process.env.URLDB = urlDB;
