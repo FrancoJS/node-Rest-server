83033892e4fc538e16d0eacd952da800a0bde836
Franco <jean.jara.fs@gmail.com>
2020-12-03 21:38:40 -0300

    Google Sing-In

--- server/config/config.js
+++ server/config/config.js
@@ -17,3 +17,8 @@ if (process.env.NODE_ENV === "dev") urlDB = "mongodb://localhost:27017/cafe";
 else urlDB = process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
+
+//Google client ID
+process.env.CLIENT_ID =
+  process.env.CLIENT_ID ||
+  "547498850869-uhnnv0d83b4r5au48j8gjfu7k31jhbbb.apps.googleusercontent.com";
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("./config/config");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //Configuracion global de rutas
 app.use(require("./routes/index"));
+//habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 mongoose
   .connect(process.env.URLDB, {
